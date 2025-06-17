371dae54a7af3541ed88e0bcb7923e7e39039b23
Jonas <jonas.lazard@student-cs.fr>
2025-06-17 11:05:22 +0200

    Starting to code the commitment scheme

---
+++ ABDLOP/commit.go
@@ -0,0 +1,217 @@
+package ABDLOP
+
+import (
+	ps "vSIS-Signature/Preimage_Sampler"
+
+	"github.com/tuneinsight/lattigo/v4/ring"
+	"github.com/tuneinsight/lattigo/v4/utils"
+)
+
+type Params struct {
+	Q     uint64
+	D     int
+	N     int
+	M1    int
+	M2    int
+	Beta1 float64
+	Beta2 float64
+}
+
+type PublicKey struct {
+	Ring   *ring.Ring
+	Params Params
+	A1     [][]*ring.Poly
+	A2     [][]*ring.Poly
+	B      [][]*ring.Poly
+	b      []*ring.Poly
+}
+
+type Commitment struct {
+	TA []*ring.Poly
+	TB [][]int64
+}
+
+type Opening struct {
+	S1 []*ring.Poly
+	S2 []*ring.Poly
+	M  []*ring.Poly
+}
+
+func KeyGen(p Params) *PublicKey {
+	ringQ, err := ring.NewRing(p.D, []uint64{p.Q})
+	if err != nil {
+		panic(err)
+	}
+	prng, _ := utils.NewPRNG()
+	uni := ring.NewUniformSampler(prng, ringQ)
+
+	A1 := make([][]*ring.Poly, p.N)
+	for i := 0; i < p.N; i++ {
+		A1[i] = make([]*ring.Poly, p.M1)
+		for j := 0; j < p.M1; j++ {
+			poly := ringQ.NewPoly()
+			uni.Read(poly)
+			ringQ.NTT(poly, poly)
+			A1[i][j] = poly
+		}
+	}
+
+	A2 := make([][]*ring.Poly, p.N)
+	for i := 0; i < p.N; i++ {
+		A2[i] = make([]*ring.Poly, p.M2)
+		for j := 0; j < p.M2; j++ {
+			poly := ringQ.NewPoly()
+			uni.Read(poly)
+			ringQ.NTT(poly, poly)
+			A2[i][j] = poly
+		}
+	}
+
+	B := make([][]*ring.Poly, p.N)
+	for i := 0; i < p.N; i++ {
+		B[i] = make([]*ring.Poly, p.M2)
+		for j := 0; j < p.M2; j++ {
+			poly := ringQ.NewPoly()
+			uni.Read(poly)
+			ringQ.NTT(poly, poly)
+			B[i][j] = poly
+		}
+	}
+
+	bvec := make([]*ring.Poly, p.N)
+	for i := 0; i < p.N; i++ {
+		poly := ringQ.NewPoly()
+		uni.Read(poly)
+		ringQ.NTT(poly, poly)
+		bvec[i] = poly
+	}
+
+	return &PublicKey{Ring: ringQ, Params: p, A1: A1, A2: A2, B: B, b: bvec}
+}
+
+func sampleGaussianVector(ringQ *ring.Ring, dim int, stddev float64) []*ring.Poly {
+	out := make([]*ring.Poly, dim)
+	dg := ps.NewDiscreteGaussian(stddev)
+	for j := 0; j < dim; j++ {
+		poly := ringQ.NewPoly()
+		for lvl, qi := range ringQ.Modulus {
+			mod := int64(qi)
+			for i := 0; i < ringQ.N; i++ {
+				x := dg.Draw(0)
+				poly.Coeffs[lvl][i] = uint64((x%mod + mod) % mod)
+			}
+		}
+		ringQ.NTT(poly, poly)
+		out[j] = poly
+	}
+	return out
+}
+
+func Commit(pk *PublicKey, s1 []*ring.Poly, m []*ring.Poly) (*Commitment, *Opening) {
+	ringQ := pk.Ring
+	s2 := sampleGaussianVector(ringQ, pk.Params.M2, pk.Params.Beta2)
+
+	tA := make([]*ring.Poly, pk.Params.N)
+	tmp := ringQ.NewPoly()
+	for i := 0; i < pk.Params.N; i++ {
+		acc := ringQ.NewPoly()
+		for j := 0; j < pk.Params.M1; j++ {
+			ringQ.MulCoeffs(pk.A1[i][j], s1[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		for j := 0; j < pk.Params.M2; j++ {
+			ringQ.MulCoeffs(pk.A2[i][j], s2[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		tA[i] = acc
+	}
+
+	tB := make([][]int64, pk.Params.N)
+	for i := 0; i < pk.Params.N; i++ {
+		acc := ringQ.NewPoly()
+		for j := 0; j < pk.Params.M2; j++ {
+			ringQ.MulCoeffs(pk.B[i][j], s2[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		ringQ.InvNTT(acc, acc)
+		coeffs := make([]int64, ringQ.N)
+		for k := 0; k < ringQ.N; k++ {
+			val := ps.UnsignedToSigned(acc.Coeffs[0][k], pk.Params.Q)
+			val += ps.UnsignedToSigned(m[i].Coeffs[0][k], pk.Params.Q)
+			coeffs[k] = val
+		}
+		tB[i] = coeffs
+	}
+
+	com := &Commitment{TA: tA, TB: tB}
+	open := &Opening{S1: s1, S2: s2, M: m}
+	return com, open
+}
+
+func polyNormInf(r *ring.Ring, p *ring.Poly, q uint64) int64 {
+	coeff := r.NewPoly()
+	r.InvNTT(p, coeff)
+	var max int64
+	for _, c := range coeff.Coeffs[0] {
+		v := ps.UnsignedToSigned(c, q)
+		if v < 0 {
+			v = -v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func Open(pk *PublicKey, com *Commitment, op *Opening) bool {
+	ringQ := pk.Ring
+
+	bound1 := 6 * pk.Params.Beta1
+	bound2 := 6 * pk.Params.Beta2
+
+	for _, p := range op.S1 {
+		if float64(polyNormInf(ringQ, p, pk.Params.Q)) > bound1 {
+			return false
+		}
+	}
+	for _, p := range op.S2 {
+		if float64(polyNormInf(ringQ, p, pk.Params.Q)) > bound2 {
+			return false
+		}
+	}
+
+	tmp := ringQ.NewPoly()
+	// recompute tA
+	for i := 0; i < pk.Params.N; i++ {
+		acc := ringQ.NewPoly()
+		for j := 0; j < pk.Params.M1; j++ {
+			ringQ.MulCoeffs(pk.A1[i][j], op.S1[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		for j := 0; j < pk.Params.M2; j++ {
+			ringQ.MulCoeffs(pk.A2[i][j], op.S2[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		if !ringQ.Equal(acc, com.TA[i]) {
+			return false
+		}
+	}
+
+	for i := 0; i < pk.Params.N; i++ {
+		acc := ringQ.NewPoly()
+		for j := 0; j < pk.Params.M2; j++ {
+			ringQ.MulCoeffs(pk.B[i][j], op.S2[j], tmp)
+			ringQ.Add(acc, tmp, acc)
+		}
+		ringQ.InvNTT(acc, acc)
+		for k := 0; k < ringQ.N; k++ {
+			val := ps.UnsignedToSigned(acc.Coeffs[0][k], pk.Params.Q)
+			val += ps.UnsignedToSigned(op.M[i].Coeffs[0][k], pk.Params.Q)
+			if val != com.TB[i][k] {
+				return false
+			}
+		}
+	}
+	return true
+}
